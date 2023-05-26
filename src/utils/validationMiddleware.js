e037b9eed00fb30fa2c9db69ae1e72ad38128cb1
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-05-26 15:20:41 -0300

    Update validation

--- src/utils/validationMiddleware.js
+++ src/utils/validationMiddleware.js
@@ -8,6 +8,12 @@ function validationMiddleware(req, res, next) {
       .send("O nome da tarefa deve ter pelo menos 3 letras.");
   }
 
+  if (name == "") {
+    return res
+      .status(400)
+      .send("O nome da tarefa não pode ser vazio.");
+  }
+
   if (state && state !== "aguardando" && state !== "andamento") {
     req.body.state = "aguardando"; // Define o estado padrão como 'aguardando' se o valor for inválido
   }
